ad11eaaab36eca29d2e02bd71b202a2d706cbc2c
Mirko Kauffman <mirkko1996@hotmail.com>
2024-10-30 17:04:50 -0300

    se establecieron las relaciones necesarias para que las tablas se comporten como espera la definicion del ejericio.

---
+++ prisma/migrations/20241030200047_tables_ralationships/migration.sql
@@ -0,0 +1,80 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `_RolePermissions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_UserPermissions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_UserRoles` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_RolePermissions" DROP CONSTRAINT "_RolePermissions_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_RolePermissions" DROP CONSTRAINT "_RolePermissions_B_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_UserPermissions" DROP CONSTRAINT "_UserPermissions_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_UserPermissions" DROP CONSTRAINT "_UserPermissions_B_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_UserRoles" DROP CONSTRAINT "_UserRoles_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_UserRoles" DROP CONSTRAINT "_UserRoles_B_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name";
+
+-- DropTable
+DROP TABLE "_RolePermissions";
+
+-- DropTable
+DROP TABLE "_UserPermissions";
+
+-- DropTable
+DROP TABLE "_UserRoles";
+
+-- CreateTable
+CREATE TABLE "UserRole" (
+    "userId" INTEGER NOT NULL,
+    "roleId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserRole_pkey" PRIMARY KEY ("userId","roleId")
+);
+
+-- CreateTable
+CREATE TABLE "UserPermission" (
+    "userId" INTEGER NOT NULL,
+    "permissionId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserPermission_pkey" PRIMARY KEY ("userId","permissionId")
+);
+
+-- CreateTable
+CREATE TABLE "RolePermission" (
+    "roleId" INTEGER NOT NULL,
+    "permissionId" INTEGER NOT NULL,
+
+    CONSTRAINT "RolePermission_pkey" PRIMARY KEY ("roleId","permissionId")
+);
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRole" ADD CONSTRAINT "UserRole_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserPermission" ADD CONSTRAINT "UserPermission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserPermission" ADD CONSTRAINT "UserPermission_permissionId_fkey" FOREIGN KEY ("permissionId") REFERENCES "Permission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermission" ADD CONSTRAINT "RolePermission_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolePermission" ADD CONSTRAINT "RolePermission_permissionId_fkey" FOREIGN KEY ("permissionId") REFERENCES "Permission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
